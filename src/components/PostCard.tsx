9cdf320c220fb55093881aab4b00bffe41602f59
JuanGomez3112 <gomezrodriguez3112@gmail.com>
2026-07-31 16:20:52 -0400

    fix(tips): ocultar botón de propina en posts propios

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -85,7 +85,7 @@ export default function PostCard({
           <IconShare className="text-[24px]" />
         </button>
         <BookmarkButton postId={post.id} initialSaved={post.savedByMe} />
-        <TipButton toUsername={post.author.username} postId={post.id} />
+        {!post.isMine && <TipButton toUsername={post.author.username} postId={post.id} />}
       </div>
 
       {/* Comentario */}
